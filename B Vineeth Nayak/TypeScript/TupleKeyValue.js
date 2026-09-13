14c7ecdca1b10aec8fe9f8d592222dd7b3ef280d
Vineeth Nayak <259198109+bvineethnayak007@users.noreply.github.com>
2026-09-13 18:40:09 +0530

    Remove generated TypeScript files

--- B Vineeth Nayak/TypeScript/TupleKeyValue.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let pair = ["age", 25];
-let anotherPair = ["score", 90];
-console.log("Key:", pair[0]);
-console.log("Value:", pair[1]);
-console.log("Key:", anotherPair[0]);
-console.log("Value:", anotherPair[1]);
